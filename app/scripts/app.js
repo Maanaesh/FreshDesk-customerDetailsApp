5137fc0621f68a8914a77e976e4d9e39cfebc4ca
Maanaesh <maanaeshswamy@gmail.com>
2024-09-19 13:28:12 +0530

    bug fix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,16 @@
 var client;
 
-
 const getHash = async (email) => {
   const finalEmail = email.trim().toLowerCase();
   const hash = CryptoJS.SHA256(finalEmail).toString();
   return hash;
 };
 
-
-const getImgUrl =(email) => {
-  const hash = getHash(email);
+const getImgUrl = async (email) => {
+  const hash = await getHash(email);  // Await the hash
   const size = 100;
   return `https://www.gravatar.com/avatar/${hash}?s=${size}&d=identicon`;
 };
-
 
 async function init() {
   client = await app.initialized();
@@ -23,13 +20,12 @@ async function init() {
 async function renderImage() {
   const avatarElement = document.getElementById('user-avatar');
   const greetingElement = document.getElementById('welcome-txt');
+  
   const contactData = await client.data.get('contact');
-  const {
-    contact: { email },
-    contact:{name},
-  } = contactData;
-  const imgUrl =getImgUrl(email); 
-  greetingElement.innerText=`Hello, ${name}`
+  const { contact: { email, name } } = contactData;
+  
+  const imgUrl = await getImgUrl(email);  // Await the image URL
+  greetingElement.innerText = `Hello, ${name}`;
   avatarElement.setAttribute('src', imgUrl);
 }
 
